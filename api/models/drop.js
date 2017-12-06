9678c280b2e647479cf3964949ef331509ccaa53
digitalbinge <dave.barr@outlook.com>
2017-12-06 12:03:58 +1000

    Product model and seed + drop

---
+++ api/models/drop.js
@@ -0,0 +1,9 @@
+const Product = require('./Product')
+
+Product.deleteMany()
+	.then(() => {
+		console.log('Wiped Products')
+	})
+	.catch((error) => {
+		console.log('Unable to delete products!', error)
+	})
