3e0661ac720895fdf23777eb117410ef86d81ba6
digitalbinge <dave.barr@outlook.com>
2017-12-06 11:50:56 +1000

    Added error handing

--- api/server.js
+++ api/server.js
@@ -11,8 +11,19 @@ const app = express()
 // Plugins 
 app.use(bodyParser.json()) // Allows me to have JSON uploads (POST/PUT)
 
-// Routes
+// JSON error hanling
+app.use((error, req, res, next) => {
+	res.send({ error: error.mesage})
+})
 
+app.use((req, res, next) => {
+	// No other routes left, must be a 404
+	res.status(404).send({
+		error: `No route found for ${req.method} ${req.url}`
+	})
+})
+
+// Routes
 app.listen(7000, (error) => {
 	if (error) {
 		console.log('There was a problem starting the server', error)
