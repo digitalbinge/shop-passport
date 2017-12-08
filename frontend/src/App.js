5c7861bf9dbc2e63bde5698377412d0d3e7fd5b6
digitalbinge <dave.barr@outlook.com>
2017-12-08 15:01:10 +1000

    Add registration from and tested all ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import axios from 'axios';
 import decodeJWT from 'jwt-decode';
 import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 import { api, setJwt } from './api/init';
 
 class App extends Component {
   state = {
     token: localStorage.getItem('token'),
-    loginError: null 
+    loginError: null,
+    register: false,
+    registerError: null
   }
 
   handleSignIn = async (event) => {
@@ -36,8 +39,56 @@ class App extends Component {
 
   }
 
+  handleRegisterForm = (event) => {
+    event.preventDefault()
+    this.setState({
+      register: true
+    })
+    console.log(this.state)
+  }
+
+  handleRegisterSubmit = async (event) => {
+    event.preventDefault()
+    const form = event.target
+    const elements = form.elements
+
+    if (elements.password.value === elements.confirmPassword.value) {
+      try {
+        
+        const response = await api.post('/auth/register', {
+            firstName: elements.firstName.value,
+            lastName: elements.lastName.value,
+            email: elements.email.value,
+            password: elements.password.value
+        })
+
+        this.setState({
+          token: response.data.token
+        })
+
+        setJwt(response.data.token)
+
+      } catch (error) {
+        this.setState({
+          registerError: error.message
+        })
+      }
+
+    } else {
+      alert('Passwords must match!')
+    }
+    
+  }
+
   render() {
     const tokesDeets = this.state.token && decodeJWT(this.state.token)
+    let displayFormHTML = null
+    if (!this.state.register) {
+      displayFormHTML = <LoginForm loginError={this.state.loginError} handleSignIn={this.handleSignIn} handleRegisterForm={this.handleRegisterForm} />
+    } else {
+      displayFormHTML = <RegisterForm handleRegisterSubmit={this.handleRegisterSubmit} />
+    }
+
 
     return (
       <div className="App">
@@ -48,7 +99,7 @@ class App extends Component {
             Your token expires {new Date(tokesDeets.exp * 1000).toLocaleString()}<br/>
           </p>
         ) : (
-          <LoginForm loginError={this.state.loginError} handleSignIn={this.handleSignIn} />
+          displayFormHTML
         )}
       </div>
     );
--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import { Input, Control, Icon, Button, Container, Box, Title } from 'reactbulma';
+// import axios from 'axios';
 
-const RegisterForm = () => (
+export default ({ handleRegisterSubmit, handleRegisterForm }) => (
+	
 	<Container>
 		<Box>
 			<Title>Login</Title>
-			<form>
-				 <Control hasIconsLeft>
-				     <Input placeholder="first name"/>
-				     <Icon right>
-				       <i className="fa fa-envelope"/>
-				     </Icon> <br /> <br />	     
-				     <Input placeholder="last name"/>
-				     <Icon right>
-				       <i className="fa fa-envelope"/>
-				     </Icon> <br /> <br />
-				     <Input placeholder="email"/>
-				     <Icon right>
-				       <i className="fa fa-envelope"/>
-				     </Icon> <br /> <br />	     
-				     <Input placeholder="password"/>
-				     <Icon right>
-				       <i className="fa fa-envelope"/>
-				     </Icon> <br /> <br />	
-				   </Control>
-				   <Button>Login</Button>
+			<form onSubmit={handleRegisterSubmit} >
+				<Control hasIconsLeft>
+			    <Input placeholder="First Name" name="firstName" type="firstName" />
+			    <Icon left>
+			      <i className="fa fa-user"/>
+			    </Icon> 
+				</Control> 
+				<Control hasIconsLeft>   
+		     	<Input placeholder="Last Name" name="lastName" type="lastName" />
+		     	<Icon left>
+		       	<i className="fa fa-user"/>
+		     	</Icon>
+				</Control> 
+				<Control hasIconsLeft>   
+		     	<Input placeholder="email" name="email" type="email" />
+		     	<Icon left>
+		       	<i className="fa fa-envelope"/>
+		     	</Icon>
+				</Control> 
+				<Control hasIconsLeft>   
+		     	<Input placeholder="password" name="password" type="password" />
+		     	<Icon left>
+		       	<i className="fa fa-key"/>
+		     	</Icon>
+				</Control>  
+				<Control hasIconsLeft>   
+		     	<Input placeholder="confirm password" name="confirmPassword" type="password" />
+		     	<Icon left>
+		       	<i className="fa fa-key"/>
+		     	</Icon>
+				</Control> 
+				<Button>Register</Button>
+				<Button onClick={handleRegisterForm}>Go To Login</Button>
 			</form>
 		</Box>
 	</Container>
 )
 
-export default RegisterForm;
